b234d4d89befc1d34e5a55de2da041f179f3e02c
RossLitz <codingclasses.049@gmail.com>
2014-09-03 21:46:11 -0500

    Stage 2.0 User part 2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:valid_attributes){
+    {
+      first_name: "Ross",
+      last_name: "litz",
+      email: "ross@example.com"
+    }
+  }
+  context "validations" do
+    let!(:user) { User.new(valid_attributes) }
+
+    before do
+      User.create(valid_attributes)
+    end
+
+    it "requires an email" do
+      expect(user).to validate_presence_of(:email)
+    end
+
+    it "requires a unique email" do
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+    it "requires a unique email (case insensitive)" do
+      user.email = "JONNYBOY@EXAMPLE.COM"
+      expect(user).to validate_uniqueness_of(:email)
+    end
+
+  end
+
+  describe "#downcase_email" do
+    it "makes the email attribute lower case" do
+      user = User.new(valid_attributes.merge(email: "JONNYBOY@EXAMPLE.COM"))
+      expect{ user.downcase_email }.to change{ user.email }.
+        from( "JONNYBOY@EXAMPLE.COM" ).
+        to( "jonnyboy@example.com" )
+    end
+
+    it "downcases an email before saving" do
+      user = User.new(valid_attributes)
+      user.email = "JONNYBOY@EXAMPLE.COM"
+      expect(user.save).to be_true
+      expect(user.email).to eq("jonnyboy@example.com")
+    end
+  end
+
 end
