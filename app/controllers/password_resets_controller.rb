35a992d27f4aa5c00a729b3a6ee15943d066941a
RossLitz <codingclasses.049@gmail.com>
2014-09-08 17:08:19 -0500

    Email Password Reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,11 @@ class PasswordResetsController < ApplicationController
   def create
       user =User.find_by(email: params[:email])
       user.generate_password_reset_token!
+      Notifier.password_reset(user).deliver
       flash[:success] = "Password email sent"
       redirect_to login_path
+  end
+  def edit
+
   end
 end
